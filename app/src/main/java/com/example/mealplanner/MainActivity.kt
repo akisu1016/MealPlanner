085622e178dededc8a0387238c744651c1d8017f
SSugimura <taipan2180151@gmail.com>
2020-07-10 11:33:44 +0900

    各機能の最初の画面生成（tabは許して） MainActivityは目次 各画面で作業してください

--- app/src/main/java/com/example/mealplanner/MainActivity.kt
+++ app/src/main/java/com/example/mealplanner/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.mealplanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Logbtn.setOnClickListener(){
+            val intentLog = Intent(this, LogActivity::class.java)
+            startActivity(intentLog)
+        }
+        Libbtn.setOnClickListener(){
+            val intentLib = Intent(this, LibActivity::class.java)
+            startActivity(intentLib)
+        }
+        Bkmbtn.setOnClickListener(){
+            val intentBkm = Intent(this, BkmActivity::class.java)
+            startActivity(intentBkm)
+        }
     }
 }

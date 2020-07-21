6ad95c05b0bcdc61410dcc0916ba381ea3e822b8
Akisu <akisu1016@yahoo.co.jp>
2020-07-21 16:38:20 +0900

    カメラと画像選択の機能実装

--- app/src/main/java/com/SquareName/mealplanner/MainActivity.kt
+++ app/src/main/java/com/SquareName/mealplanner/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.SquareName.mealplanner
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.SquareName.mealplanner.tflite.Classifier
+import com.SquareName.mealplanner.tflite.Classifier.create
+import com.SquareName.mealplanner.ui.Libarary.LibararyFragment
+import kotlinx.android.synthetic.main.fragment_library.*
+import java.io.FileDescriptor
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+    private lateinit var classifier: Classifier
+    private lateinit var Libarary: LibararyFragment
+    val RESULT_IMAGEFILE = 1001
+    val RESULT_CAMERAFILE = 1002
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +47,97 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        try {
+            Libarary = LibararyFragment()
+            classifier = create(this, Classifier.Device.CPU, 2)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        val newRequestCode = requestCode and 0xffff
+        textView = findViewById<TextView>(R.id.resulttextView)
+        imageView = findViewById<ImageView>(R.id.imageView)
+
+        //終了リザルトが画像選択アクテビティ
+        if (newRequestCode == RESULT_IMAGEFILE && resultCode == Activity.RESULT_OK && resultData != null) {
+            var uri: Uri? = resultData.data
+            var pfDescriptor = getContentResolver().openFileDescriptor(uri!!, "r")
+            val fileDescriptor: FileDescriptor = pfDescriptor!!.fileDescriptor
+            val bmp = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+            pfDescriptor.close()
+
+            this.imageView.setImageBitmap(resizeImage(bmp))
+
+            var results =
+                classifier.recognizeImage(resizeImage(bmp), 1)
+            var text: String? = ""
+
+            for (result in results) {
+                val title = result.title
+                text += """
+                                    $title
+
+                                    """.trimIndent()
+                Log.d("array", title)
+            }
+            textView.text = text
+
+        //終了リザルトがカメラアクテビティ
+        } else if (newRequestCode == RESULT_CAMERAFILE && resultCode == Activity.RESULT_OK && resultData != null) {
+            val bmp: Bitmap
+            if (resultData.extras == null) {
+                return
+            } else {
+                bmp = resultData.extras!!["data"] as Bitmap
+
+                this.imageView.setImageBitmap(resizeImage(bmp))
+
+                var results =
+                    classifier.recognizeImage(resizeImage(bmp), 1)
+                var text: String? = ""
+
+                for (result in results) {
+                    val title = result.title
+                    text += """
+                                    $title
+
+                                    """.trimIndent()
+                    Log.d("array", title)
+                }
+                this.resulttextView.text = text
+            }
+        }
+    }
+
+
+    //ビットマップイメージをリサイズ
+    fun resizeImage(bmp: Bitmap): Bitmap {
+        var height = bmp.height
+        var width = bmp.width
+        while (true) {
+            var i = 2
+            if (width < 500 && height < 500) {
+                break
+            } else {
+                if (width > 500 || height > 500) {
+                    width = width / i
+                    height = height / i
+                } else {
+                    break
+                }
+                i++
+            }
+        }
+
+        var croppedBitmap =
+            Bitmap.createScaledBitmap(bmp, width, height, false)
+
+        return croppedBitmap
+
     }
 }
--- app/src/main/java/com/SquareName/mealplanner/ui/Libarary/LibararyFragment.kt
+++ app/src/main/java/com/SquareName/mealplanner/ui/Libarary/LibararyFragment.kt
@@ -1,31 +1,58 @@
 package com.SquareName.mealplanner.ui.Libarary
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.SquareName.mealplanner.MainActivity
 import com.SquareName.mealplanner.R
+import com.SquareName.mealplanner.tflite.Classifier
+import com.SquareName.mealplanner.tflite.Classifier.create
+import kotlinx.android.synthetic.main.fragment_library.*
+import kotlinx.android.synthetic.main.fragment_library.view.*
+import java.io.FileDescriptor
+import java.io.IOException
 
 class LibararyFragment : Fragment() {
 
-    private lateinit var libraryViewModel: LibraryViewModel
+    val RESULT_IMAGEFILE = 1001
+    val RESULT_CAMERAFILE = 1002
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        libraryViewModel =
-            ViewModelProviders.of(this).get(LibraryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_library, container, false)
-        val textView: TextView = root.findViewById(R.id.text_library)
-        libraryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        val view = inflater.inflate(R.layout.fragment_library, container, false)
+
+
+        view.camera_button.setOnClickListener{
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, RESULT_CAMERAFILE)
+        }
+
+        view.imagebutton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "image/*"
+            startActivityForResult(intent, RESULT_IMAGEFILE)
+        }
+
+        return view
     }
+
 }

d441535fc9e103494298bb44c0ceb161be213c60
Akisu <akisu1016@yahoo.co.jp>
2020-07-28 19:38:35 +0900

    必要のないコメントアウトの削除

--- app/src/main/java/com/SquareName/mealplanner/ui/Recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/SquareName/mealplanner/ui/Recyclerview/RecyclerAdapter.kt
@@ -12,9 +12,6 @@ import kotlinx.android.synthetic.main.list_item.view.*
 
 class RecyclerAdapter(private val customList: Array<String>, private val listener: OnItemClickListener) : RecyclerView.Adapter<RecyclerViewHolder>(){
 
-    // リスナー格納変数
-//    private lateinit var listener: OnItemClickListener
-
     // getItemCount onCreateViewHolder onBindViewHolderを実装
     // 上記のViewHolderクラスを使ってViewHolderを作成
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
@@ -42,53 +39,4 @@ class RecyclerAdapter(private val customList: Array<String>, private val listene
         fun onItemClick(view: View, position: Int, clickedText: String)
     }
 
-    // リスナー
-//    fun setOnItemClickListener(listener: OnItemClickListener) {
-//        this.listener = listener
-//    }
-
 }
-
-
-//class RecyclerAdapter(private val context: Context, private val itemClickListener: Unit, private val itemList:List<String>)
-//    : RecyclerView.Adapter<RecyclerViewHolder>() {
-//
-//    private var mRecyclerView : RecyclerView? = null
-//
-//    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
-//        super.onAttachedToRecyclerView(recyclerView)
-//        mRecyclerView = recyclerView
-//    }
-//
-//    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
-//        super.onDetachedFromRecyclerView(recyclerView)
-//        mRecyclerView = null
-//
-//    }
-//
-//    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-//        holder?.let {
-//            it.itemTextView.text = itemList.get(position)
-//            it.itemImageView.setImageResource(R.mipmap.ic_launcher)
-//        }
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return itemList.size
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-//
-//        val layoutInflater = LayoutInflater.from(context)
-//        val mView = layoutInflater.inflate(R.layout.list_item, parent, false)
-//
-//        mView.setOnClickListener { view ->
-//            mRecyclerView?.let {
-//                itemClickListener.onItemClick(view, it.getChildAdapterPosition(view))
-//            }
-//        }
-//
-//        return RecyclerViewHolder(mView)
-//    }
-//
-//}
